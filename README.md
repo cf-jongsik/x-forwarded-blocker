063848d7faad91c2cc76d7027c21ea4c3025df1a
Jongsik <jongsik@cloudflare.com>
2024-02-21 08:52:23 +0900

    init

---
+++ README.md
@@ -0,0 +1,61 @@
+<!-- PROJECT SHIELDS -->
+<!--
+*** I'm using markdown "reference style" links for readability.
+*** Reference links are enclosed in brackets [ ] instead of parentheses ( ).
+*** See the bottom of this document for the declaration of the reference variables
+*** for contributors-url, forks-url, etc. This is an optional, concise syntax you may use.
+*** https://www.markdownguide.org/basic-syntax/#reference-style-links
+-->
+
+<br />
+
+<h3 align="center">x-forwarded-for-blocker</h3>
+
+  <p align="center">
+    using Cloudflare Workers to create a rule based on a ENV variable
+    the example code blocks every traffic contains IPs from IPTOBLOCK environment variable
+	modify the code to fit your needs
+    <br />
+
+<!-- GETTING STARTED -->
+
+## Getting Started
+
+### Prerequisites
+
+1. create token from Cloudflare account with following access
+   Zone WAF : edit
+   Firewall Service : edit
+
+2. find your Zone ID from dashboard
+
+### Installation
+
+1. login to your Cloudflare profile
+   (the account must have an edit access from the zone you are modifying)
+
+2. Clone the repo
+   ```sh
+   git clone https://github.com/cf-jongsik/x-forwarded-for-blocker.git
+   ```
+3. Install NPM packages
+   ```sh
+   npm install
+   ```
+4. login
+   ```sh
+   npm exec wrangler login
+   ```
+5. modify variable in wrangler.toml
+6. deploy
+   ```sh
+   npm exec wrangler deploy
+   ```
+7. add secret variable (API TOKEN + ZONE ID)
+   ```sh
+   npm exec wrangler secret put CLOUDFLARE_API_TOKEN
+   ```
+   ```sh
+   npm exec wrangler secret put ZONEID
+   ```
+   <p align="right">(<a href="#readme-top">back to top</a>)</p>
--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,113 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+import { Hono } from 'hono';
 
-export interface Env {
-	// Example binding to KV. Learn more at https://developers.cloudflare.com/workers/runtime-apis/kv/
-	// MY_KV_NAMESPACE: KVNamespace;
-	//
-	// Example binding to Durable Object. Learn more at https://developers.cloudflare.com/workers/runtime-apis/durable-objects/
-	// MY_DURABLE_OBJECT: DurableObjectNamespace;
-	//
-	// Example binding to R2. Learn more at https://developers.cloudflare.com/workers/runtime-apis/r2/
-	// MY_BUCKET: R2Bucket;
-	//
-	// Example binding to a Service. Learn more at https://developers.cloudflare.com/workers/runtime-apis/service-bindings/
-	// MY_SERVICE: Fetcher;
-	//
-	// Example binding to a Queue. Learn more at https://developers.cloudflare.com/queues/javascript-apis/
-	// MY_QUEUE: Queue;
-}
-
-export default {
-	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
-		return new Response('Hello World!');
-	},
+type Bindings = {
+	CLOUDFLARE_API_TOKEN: string;
+	ZONEID: string;
+	ACCOUNTID: string;
+	IPTOBLOCK: string;
 };
+
+type filterType = {
+	result: [
+		{
+			id: string;
+			expression: string;
+			paused: boolean;
+		}
+	];
+	success: boolean;
+	errors: [];
+	messages: [];
+};
+const app = new Hono<{ Bindings: Bindings }>();
+
+app.get('/', async (c) => {
+	if (!c.env.IPTOBLOCK) {
+		return c.text('no IPs provided, update the variable: IPTOBLOCK', 400);
+	}
+
+	const ips = c.env.IPTOBLOCK.split(',');
+	// test line
+	//const ips = '5.5.5.5,6.6.6.6,7.7.7.7'.split(',');
+	const base: string = `https://api.cloudflare.com/client/v4/zones/${c.env.ZONEID}`;
+	const ruleURL: URL = new URL(base + '/firewall/rules');
+	const filterURL: URL = new URL(base + '/filters');
+	let conditions: string = '';
+	for (const ip of ips) {
+		// '(http.x_forwarded_for contains "1.1.1.1") or (http.x_forwarded_for contains "2.2.2.2")';
+		if (!conditions) {
+			conditions += `(http.x_forwarded_for contains "${ip}")`;
+		} else {
+			conditions += ` or (http.x_forwarded_for contains "${ip}")`;
+		}
+	}
+
+	const expressionData = {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json',
+			Authorization: 'Bearer ' + c.env.CLOUDFLARE_API_TOKEN,
+		},
+		body: JSON.stringify([{ expression: conditions }]),
+	};
+	console.log(expressionData);
+	const filterRes = await fetch(filterURL, expressionData);
+	const filterJson: filterType = await filterRes.json();
+
+	const rules = [
+		{
+			filter: {
+				id: filterJson.result[0].id,
+			},
+			action: 'block',
+			description: 'BLOCK XFF IPS created by Workers',
+		},
+	];
+	const ruleData = {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json',
+			Authorization: 'Bearer ' + c.env.CLOUDFLARE_API_TOKEN,
+		},
+		body: JSON.stringify(rules),
+	};
+	return fetch(ruleURL, ruleData);
+});
+
+app.get('/filters/:id?', async (c) => {
+	const header = {
+		headers: {
+			'Content-Type': 'application/json',
+			Authorization: 'Bearer ' + c.env.CLOUDFLARE_API_TOKEN,
+		},
+	};
+	const base: string = `https://api.cloudflare.com/client/v4/zones/${c.env.ZONEID}`;
+	let url: URL;
+	const id = c.req.param('id');
+	if (!id) {
+		url = new URL(base + `/filters`);
+	} else {
+		url = new URL(base + `/filters/${id}`);
+	}
+	return fetch(url, header);
+});
+
+app.get('/rules/:id?', async (c) => {
+	const header = {
+		headers: {
+			'Content-Type': 'application/json',
+			Authorization: 'Bearer ' + c.env.CLOUDFLARE_API_TOKEN,
+		},
+	};
+	const base: string = `https://api.cloudflare.com/client/v4/zones/${c.env.ZONEID}`;
+	let url: URL;
+	const id = c.req.param('id');
+	if (!id) {
+		url = new URL(base + `/firewall/rules`);
+	} else {
+		url = new URL(base + `/firewall/rules/${id}`);
+	}
+	return fetch(url, header);
+});
+
+export default app;
